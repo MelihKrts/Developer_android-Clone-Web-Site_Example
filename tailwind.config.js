4bb10a19c17b218a5f2fb230bc3ae286d3b43a3a
MelihKrts <melih.karatas1281@gmail.com>
2023-04-11 13:33:53 +0300

    Finish Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
-      
+      screens: {
+        'xs': '475px',
+        'xxs':'300px'
+      },
     },
   },
   plugins: [],
